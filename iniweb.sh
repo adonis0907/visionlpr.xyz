5a57347332b38d248101353b851a37e224731a51
Alexis Colin <adonis0907@outlook.com>
2022-01-14 05:10:17 +0900

    initial commit

---
+++ iniweb.sh
@@ -0,0 +1,6 @@
+cd /home/lpr-web/lpr-web
+sleep 20
+sudo git pulls
+sleep 5
+npm start
+
---
+++ src/Services/CityApi.js
@@ -0,0 +1,49 @@
+const create = city =>{
+    return fetch('/city/create',{
+        method: 'post',
+        body: JSON.stringify(city),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res =>res.json())
+        .then(data => data);
+};
+
+const fetchAll = () =>{
+    return fetch('/city/fetchAll', {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res =>res.json())
+        .then(data => data);
+};
+
+const update = city =>{
+    return fetch('/city/update', {
+        method: 'put',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(city)
+    }).then(res =>res.json())
+        .then(data => data);
+};
+
+const deleteCity = cities =>{
+    return fetch('/city/delete', {
+        method: 'delete',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({cities: cities})
+    }).then(res =>res.json())
+        .then(data => data);
+};
+
+export default {
+    create: create,
+    fetchAll: fetchAll,
+    update: update,
+    deleteCity: deleteCity
+}
